0855d9282da6a130dd9b73fe124c66d29364bd03
heyimlilyx3 <msalexawill@gmail.com>
2026-09-21 12:43:16 -0700

    final commit

--- lab-13/main.cpp
+++ lab-13/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <cmath>
 
 using namespace std;
 
@@ -19,6 +20,46 @@ void selectionSort(array<student, 150>& students) {
         }
         swap(students[i], students[minIndex]);
     }
+}
+
+void printMin(array<student, 150>& students) {
+    cout << "Minimum score: " << students[0].score << " (ID: " << students[0].id << ")" << endl;
+}
+void printMax(array<student, 150>& students) {
+    cout << "Maximum score: " << students[149].score << " (ID: " << students[149].id << ")" << endl;
+}
+void printMean(array<student, 150>& students) {
+    double sum = 0;
+    for (const auto& s : students) {
+        sum += s.score;
+    }
+    double mean = sum / 150;
+    cout << "Mean score: " << mean << endl;
+}
+void printMedian(array<student, 150>& students) {
+    double median;
+    if (150 % 2 == 0) {
+        median = (students[74].score + students[75].score) / 2.0;
+    } else {
+        median = students[75].score;
+    }
+    cout << "Median score: " << median << " (ID: " << students[75].id << ")" << endl;
+}
+void printStandardDeviation(array<student, 150>& students) {
+    double mean = 0;
+    for (const auto& s : students) {
+        mean += s.score;
+    }
+    mean /= 150;
+
+    double variance = 0;
+    for (const auto& s : students) {
+        variance += (s.score - mean) * (s.score - mean);
+    }
+    variance /= 150;
+
+    double stddev = sqrt(variance);
+    cout << "Standard Deviation: " << stddev << endl;
 }
 
 int main() {
@@ -52,6 +93,13 @@ int main() {
     }
 
     outputFile.close();                                     // Close the output file
+
+
+    printMin(students);
+    printMax(students);
+    printMean(students);
+    printMedian(students);
+    printStandardDeviation(students);
 
     return 0;
 }
